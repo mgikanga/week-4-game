2ae4c53641e2bae7732ada12fd7c6fa72379ece4
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-03-18 21:12:37 -0700

    added images and game instructions in the html file

---
+++ assets/javascript/game.js
@@ -0,0 +1,16 @@
+console.log("hey");
+var min = 1;
+var max = 12;
+var num;
+function random1l(min, max) {
+    var num = Math.floor(Math.random() * (max - min + 1) + min);
+    return num;
+    
+}
+console.log(num);
+
+
+
+/*$( "h1" ).click(function() {
+  console.log("hi");
+  });*/
