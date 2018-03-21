1d1c227d4551a9b833a6a3f6c8ab59100568935b
Monicah Gikanga <mgikanga@Admins-MacBook-Air.local>
2018-03-21 11:37:52 -0700

    added random number generator for the target number and user input,

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,16 +1,60 @@
 console.log("hey");
-var min = 1;
-var max = 12;
-var num;
-function random1l(min, max) {
-    var num = Math.floor(Math.random() * (max - min + 1) + min);
-    return num;
+$(document).ready(function () {
+    // my variables
+    var wins = 0;
+    var losses = 0;
+    var myCounter = 0;
+    var value;
+    var min = 19;
+    var max = 120;
+    var theNumber;
+    var button1;
+    var button2;
+    var button3;
+    var button4;
+    // generate the target number
+    function numb() {
+        theNumber = Math.floor(Math.random() * (max - min + 1) + min);
+        console.log(theNumber);
+    }
+    numb();
+    $("#numb").text("Target: " + theNumber);
+
+    // generate the random value of the gems
+    var min = 1;
+    var max = 12;
+    button1 = Math.floor(Math.random() * (max - min + 1) + min);
+    $("#gem1").val(button1);
+    console.log(button1);
+     button2 = Math.floor(Math.random() * (max - min + 1) + min);
+     $("#gem2").val(button2);
+    console.log(button2);
+     button3 = Math.floor(Math.random() * (max - min + 1) + min);
+     $("#gem3").val(button3);
+     console.log(button3);
+     button4= Math.floor(Math.random() * (max - min + 1) + min);
+     $("#gem4").val(button4);
+     console.log(button4);
     
-}
-console.log(num);
+    //on click add the numbers
+    $(".gems").click(function () {
+        myCounter += value;
+        $("#yourscore").text("Total Score: " + myCounter);
+        // compare the targer number and the counter
+        if (myCounter === theNumber) {
+            wins++;
+            $("#winnig").text("Wins: " + wins);
 
+        }
+        // add up the value for every click
+        //var totalScore += num1; 
+        //console.log(value);
+    });
 
+    var reset = function () {
+        var wins = 0;
+        var losses = 0;
+        var myCounter = 0;
+    };
 
-/*$( "h1" ).click(function() {
-  console.log("hi");
-  });*/
+});
